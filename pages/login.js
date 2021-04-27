2ed7726893be9aa1fce6af5931af11e1771f33af
Victor2752 <65194770+Victor2752@users.noreply.github.com>
2021-04-27 08:45:37 -0300

    alterações minimas

--- pages/login.js
+++ pages/login.js
@@ -12,12 +12,15 @@ function Login () {
                         sig-in
                     </h1>
                     <div className={styles.loginBox}>
-                        <input type='text' value='E-mail'/>
-                        <input type='text' value='Senha'/>
+                        <input type='text' value='E-mail' className={styles.mailBox}/>
+                        <input type='text' value='Senha' className={styles.passwordBox}/>
                     </div>
                     <button className={styles.btnEntra}>
                         Entrar
                     </button>
+                    <a href="/" className={styles.link}>
+                        <p>esqueceu a senha? &rarr;</p>
+                    </a>
                 </div>
             </div>
         </main>
